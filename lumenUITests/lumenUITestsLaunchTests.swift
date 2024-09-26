40d89d64a12c7bbeaa88c83aac01e3aeadfe299f
Jeremiah Lena <jeremiahlena13@gmail.com>
2024-09-26 16:52:13 +0100

    Initial Commit

---
+++ lumenUITests/lumenUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  lumenUITestsLaunchTests.swift
+//  lumenUITests
+//
+//  Created by Jeremiah Lena on 26/09/2024.
+//
+
+import XCTest
+
+final class lumenUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
